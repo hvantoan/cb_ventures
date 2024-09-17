26a252cd1c6ec540c611e39de8fc005b85c0d23f
Hồ Văn Toàn <it.vantoan@gmail.com>
2024-09-17 10:28:23 +0700

    Fix: missing props

--- apps/ventures/src/app/(authenticated)/(category)/bots/[botId]/page.tsx
+++ apps/ventures/src/app/(authenticated)/(category)/bots/[botId]/page.tsx
@@ -1,10 +1,13 @@
+import { useToggle } from '@fumy/utilities/hooks';
+
 import BotInfoHeader from '../_components/bot-info-header';
 import { BotDetailCard } from './_components';
 
 const BotDetailPage: React.FC<Params<'botId'>> = async ({ params: { botId } }) => {
+  const { isOpen: isLoading, handleClose: stopLoading, handleOpen: startLoading } = useToggle();
   return (
     <>
-      <BotInfoHeader botId={botId} />
+      <BotInfoHeader botId={botId} isLoading={isLoading} startLoading={startLoading} stopLoading={stopLoading} />
       <BotDetailCard botId={botId} />
     </>
   );
